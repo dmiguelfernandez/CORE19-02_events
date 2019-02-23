5bf7d2c3653c490e661aa8c14e7158c50c6f48b7
Daniel Miguel Fernandez <dmiguelfernandez@gmail.com>
2019-02-23 21:21:10 +0100

    parte nueva

--- practica2.js
+++ practica2.js
@@ -1,8 +1,7 @@
-
 const Habitacion = require('./habitacion');
 const Climatizador = require('./climatizador');
 const Termostato = require('./termostato');
-
+const Programador = require('./programador');
 // Creamos una habitacion:
 const dormitorio = new Habitacion();
 dormitorio.temperatura = 22;
@@ -13,16 +12,33 @@ const climatizador = new Climatizador(dormitorio);
 // Creamos un Termostato que mira la temperatura de la habitacion:
 const termostato = new Termostato(dormitorio);
 
+//Configuramos el programador
+const ArrayProgramador =[
+    {hora: "07:00",
+    temperatura: 22
+    },
+    {hora :"08:30",
+        temperatura:18
+    },
+    {hora: "18:00",
+    temperatura:22
+    },
+    {hora: "21:11",
+    temperatura: 30
+    }
+];
+//Creamos un Programador para controlar la temperatura de la habitación
+const programador = new Programador(ArrayProgramador);
+
 // Configuramos el termostato para controlar la temperatura:
 termostato.on('muchofrio', () => climatizador.calentar());
 termostato.on('muchocalor', () => climatizador.enfriar());
 
 // Mostar la temperatura periodicamente:
 termostato.on('tic', (temp) => console.log(`${temp.toFixed(1)}ºC`));
-
-// Configurar la temp ideal a 20 grados:
+programador.on('ideal',(tempIdeal) => termostato.indicarTemperaturaIdeal(tempIdeal));
+// Configurar la temp ideal a 20 grados
 termostato.indicarTemperaturaIdeal(20);
 
 // Encender el termostato:
 termostato.encender();
-
---
+++ programador.js
@@ -0,0 +1,28 @@
+// Importar modulo Later.js:
+const later = require('later');
+const EventEmitter = require('./events');
+
+class Programador extends EventEmitter{
+    constructor(configuracion) {
+      super();
+    later.date.localTime();
+
+    if (configuracion instanceof Array){
+      for(var i=0; i<configuracion.lenght;i++){
+        var pos =ArrayHora[i];
+
+        console.log("Hora"+ pos.hora + "temperaturaIdeal")
+        later.setInterval(
+          () => this.emit('ideal', pos.temperatura),
+          later.parse.text ('at' + pos.hora)
+        );
+      }
+    }
+    // Crear planificación para todos los dias a las 18:00
+    const sched = later.parse.text('at 18:00');
+
+
+	}
+}
+
+exports = module.exports = Programador;
